2a7821a41144ce4ab22a82c231553cd219432d73
Felix <felixlenain@yahoo.fr>
2020-02-21 16:29:49 +0100

    Base de donnée utilisateur, formulaire de connexion utilisateur

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,18 +2,49 @@
 
 namespace App\DataFixtures;
 use App\Entity\Ad;
-use App\Entity\Image;
 use Faker\Factory;
+use App\Entity\User;
+use App\Entity\Image;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
-
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }  
+
     public function load(ObjectManager $manager)
     {
         $faker = Factory::create('FR-fr');
+
+        //nous gerons les utilisateurs
+        $users=[];
+        $genres=["male","female"];
+
+        for ($i=1;$i<15 ; $i++)
+        {
+            $user = new User();
+
+            $genre = $faker->randomElement($genres);
+
+            $hash = $this->encoder->encodePassword($user, 'password');
+
+            $user->setFirstName($faker->firstName($genres));
+            $user->setLastName($faker->lastName);
+            $user->setEmail($faker->email);
+            $user->setPicture('https://randomuser.me/api/portraits/' . ($genre=='male' ? 'men/' : 'women/'. mt_rand(1,99)).'.jpg');
+            $user->setHash($hash);
+            $user->setIntroduction($faker->sentence(8));
+            $user->setDescription('<p>'.join('</p><p>',$faker->paragraphs(3)).'</p>');
+            
+            $manager->persist($user);
+            $users [] = $user;
+        }
         
+        //nous gérons les annonces
         for($i = 1; $i<30; $i++)
         {
             $ad =new Ad();
@@ -24,12 +55,15 @@ class AppFixtures extends Fixture
             $intro = $faker->paragraph(2);
             $content = '<p>'.join('</p><p>',$faker->paragraphs(5)).'</p>';
 
+            $user = $users[mt_rand(0, count($users)-1)];
+
             $ad->setTitle($title);
             $ad->setPrice(mt_rand(35, 250));
             $ad->setIntroduction($intro);
             $ad->setContent($content);
             $ad->setCoverImage($coverImage);
             $ad->setRooms(mt_rand(1, 4));
+            $ad->setAuthor($user);
             
             for($j = 1; $j<= mt_rand(2,5); $j++)
             {
